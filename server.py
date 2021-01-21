2da7200fa4aae12326e041e7510902456941b304
Marissa Aguilera <marissaguilera@yahoo.com>
2021-01-21 12:42:11 -0800

    Changes to server.py

--- server.py
+++ server.py
@@ -1,44 +1,33 @@
 """Server for portfolio."""
 
 from flask import (Flask, render_template, jsonify, request, redirect)
+# from model import connect_to_db
+# import crud
 
+from jinja2 import StrictUndefined
+
+
+
+"""Server for portfolio app."""
 app = Flask(__name__) 
+app.secret_key = "portfolio"
+app.jinja_env.undefined = StrictUndefined
 
-@app.route("/")
+
+
+@app.route("/", methods = ["GET"])
 def homepage():
     """View a homepage."""
 
     return render_template("root.html")
 
-
-
-@app.route("/projects")
-def projects_page():
-    """View all projects."""
-
-    return render_template("root.html")
-
-
-
-@app.route("/resume")
-def resume_page():
-    """View resume."""
-    #view or download resume? 
-
-    return render_template("root.html")
-
-
-
-@app.route("/contact")
-def contact_page():
-    """View listed contact information."""
-    #linkedin, github, email, phone number
-
-    return render_template("root.html")
+#resume, projects, contact info 
 
 
 
 
 if __name__ == '__main__':
+    connect_to_db(app)
+    # app.run()
     app.run(debug=True, host='0.0.0.0')
     #change to app.run() when complete 
